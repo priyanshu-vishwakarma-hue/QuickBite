7c0c2fb52adb0a6fd60dcb01db306c66cc87fb2c
priyanshu-vishwakaram-hue <pvish8825@gmail.com>
2025-08-07 02:28:09 +0530

    final commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwind from '@tailwindcss/vite';
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react(), tailwind()]
+});
